4d589a351222f7e36d54a32a3ae058de8a15a4b8
Iwonnet <nowiluk@gmail.com>
2018-08-04 10:59:25 +0200

    changes

--- src/main/java/it/stacja/app/factory/model/Car.java
+++ src/main/java/it/stacja/app/factory/model/Car.java
@@ -1,9 +1,13 @@
 package it.stacja.app.factory.model;
 
 import com.google.common.collect.Lists;
+import it.stacja.app.factory.context.Component;
+import it.stacja.app.factory.context.Prototype;
 
 import java.util.List;
 
+@Component
+@Prototype
 public class Car {
 
     private List<Wheel> wheels;
--- src/main/java/it/stacja/app/factory/model/Wheel.java
+++ src/main/java/it/stacja/app/factory/model/Wheel.java
@@ -1,8 +1,10 @@
 package it.stacja.app.factory.model;
 
 import it.stacja.app.factory.context.Component;
+import it.stacja.app.factory.context.Prototype;
 
 @Component
+@Prototype
 public class Wheel {
     private Tire tire;
 
