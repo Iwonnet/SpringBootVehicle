f4bb2b773ac051decd9ca44021466ee7b0e2dca5
Iwonnet <nowiluk@gmail.com>
2018-08-04 10:27:46 +0200

    Add List of components.class

--- src/main/java/it/stacja/app/factory/context/SimpleContext.java
+++ src/main/java/it/stacja/app/factory/context/SimpleContext.java
@@ -11,18 +11,24 @@ import java.util.Set;
 
 public class SimpleContext {
 
-  private final String prefix = null;
+  private final String prefix;
 
-  public SimpleContext() {
+  public SimpleContext(String prefix) {
+    this.prefix = prefix;
     init();
   }
 
   private void init() {
     Reflections reflections = new Reflections(this.prefix);
 
+    Set<Class<?>> annotatedWith = reflections.getTypesAnnotatedWith(Component.class);
+    annotatedWith.stream().forEach(aClass -> resolve(aClass));
+
   }
 
   private Object resolve(Class<?> classType) {
+
+
 
     return null;
   }
