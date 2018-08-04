d9fa33c7be144fd53284c8ddd2619f1470549ed4
Iwonnet <nowiluk@gmail.com>
2018-08-04 15:07:48 +0200

    REST-controller-wykopservice

---
+++ src/main/java/it/stacja/app/web/wykop/app/DefaultWykopService.java
@@ -0,0 +1,19 @@
+package it.stacja.app.web.wykop.app;
+
+import it.stacja.app.web.wykop.domain.Wykop;
+import org.springframework.stereotype.Component;
+
+import java.util.List;
+
+@Component
+public class DefaultWykopService implements WykopService {
+    @Override
+    public void createWykop() {
+
+    }
+
+    @Override
+    public List<Wykop> selectAll() {
+        return null;
+    }
+}
