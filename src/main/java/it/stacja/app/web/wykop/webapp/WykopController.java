22b7139abfb9338321c0b73e4f2e305afecfec3f
Iwonnet <nowiluk@gmail.com>
2018-08-04 15:46:12 +0200

    REST-controller-wykoprequest-change7

--- src/main/java/it/stacja/app/web/wykop/webapp/WykopController.java
+++ src/main/java/it/stacja/app/web/wykop/webapp/WykopController.java
@@ -1,13 +1,26 @@
 package it.stacja.app.web.wykop.webapp;
 
+
+import it.stacja.app.web.wykop.app.WykopService;
+import it.stacja.app.web.wykop.domain.Wykop;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import java.util.List;
+import java.util.Map;
+
 @Controller
 @RequestMapping("/wykop")
 public class WykopController {
+
+
+    private WykopService wykopService;
+
+    public void setWykopService(WykopService wykopService) {
+        this.wykopService = wykopService;
+    }
 
     @GetMapping("/main")
     public void main(){
@@ -15,7 +28,16 @@ public class WykopController {
     }
 
     @PostMapping("/add")
-    public void add(CreateWykopRequest request){
+    public String add(CreateWykopRequest request){
+        wykopService.createWykop(request);
 
+        return "redirect:list";
+
+    }
+
+    @GetMapping("/list")
+    public void list(Map pageMap){
+        List<Wykop> list = wykopService.selectAll();
+        pageMap.put("list",list);
     }
 }
