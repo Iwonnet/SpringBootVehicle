8f56d128abb35214f0502b38aae1b0f47d946e05
Iwonnet <nowiluk@gmail.com>
2018-08-04 15:28:32 +0200

    REST-controller-wykoprequest-change5

--- src/main/java/it/stacja/app/web/wykop/app/DefaultWykopService.java
+++ src/main/java/it/stacja/app/web/wykop/app/DefaultWykopService.java
@@ -1,6 +1,7 @@
 package it.stacja.app.web.wykop.app;
 
 import it.stacja.app.web.wykop.domain.Wykop;
+import it.stacja.app.web.wykop.infrastructure.WykopRepository;
 import it.stacja.app.web.wykop.webapp.CreateWykopRequest;
 import org.springframework.stereotype.Component;
 
@@ -8,6 +9,15 @@ import java.util.List;
 
 @Component
 public class DefaultWykopService implements WykopService {
+
+    private WykopFactory wykopFactory;
+    private WykopRepository wykopRepository;
+
+    public DefaultWykopService(WykopFactory wykopFactory, WykopRepository wykopRepository) {
+        this.wykopFactory = wykopFactory;
+        this.wykopRepository = wykopRepository;
+    }
+
     @Override
     public void createWykop(CreateWykopRequest request) {
 
