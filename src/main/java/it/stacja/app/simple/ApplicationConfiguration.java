cff2a630ea258cd13fa0bcdd53e7c53f86a080c0
Iwonnet <nowiluk@gmail.com>
2018-08-04 12:22:42 +0200

    changes

---
+++ src/main/java/it/stacja/app/simple/ApplicationConfiguration.java
@@ -0,0 +1,9 @@
+package it.stacja.app.simple;
+
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class ApplicationConfiguration {
+
+
+}
--- src/main/java/it/stacja/app/simple/SimpleSpringExample.java
+++ src/main/java/it/stacja/app/simple/SimpleSpringExample.java
@@ -1,9 +1,15 @@
 package it.stacja.app.simple;
 
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
 public class SimpleSpringExample {
 
 
     public static void main(String[] args) {
+
+
+        ApplicationContext ctx = new AnnotationConfigApplicationContext(ApplicationConfiguration.class);
 
     }
 
